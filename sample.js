bcc766cb26d84766c77144d4aa81971839ece758
sarathmunusamy <sarath.munusamy@aspiresys.com>
2016-10-13 09:39:29 +0530

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,34 @@
+//sample
+//sample//sample
+//sample//sample
+//sample//sample
+//sample//sample
+//sample//sample
+//sample//sample
+//sample
+var unique = function(origArr) {
+    var newArr = [],
+        origLen = origArr.length,
+        found, x, y;
+
+    for (x = 0; x < origLen; x++) {
+        found = undefined;
+        for (y = 0; y < newArr.length; y++) {
+            if (origArr[x] === newArr[y]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            newArr.push(origArr[x]);
+        }
+    }
+    return newArr;
+}
+
+var arr = ['a', 'b', 'c', 'a', 'd', 'e', 'b', 'c', 'f', 'g', 'f'];
+
+var arrUnique = unique(arr);
+var arrUnique = unique(arr);
+
+alert(arrUnique);
